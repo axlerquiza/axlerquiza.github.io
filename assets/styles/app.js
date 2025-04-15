feb286269e7965c203b1f2376d669c7491d38c23
Axl Erquiza <axl.erquiza@gmail.com>
2025-04-16 00:25:42 +0800

    update links

--- assets/styles/app.js
+++ assets/styles/app.js
@@ -1,14 +1,14 @@
-function certificationLinks() {
-    window.open('https://aws.amazon.com/certification/certified-cloud-practitioner/', '_blank');
-    window.open('https://aws.amazon.com/certification/certified-data-engineer-associate/', '_blank');
-}
+// function certificationLinks() {
+//     window.open('https://aws.amazon.com/certification/certified-cloud-practitioner/', '_blank');
+//     window.open('https://aws.amazon.com/certification/certified-data-engineer-associate/', '_blank');
+// }
 
-function otherLinks() {
-    window.open('https://h.43z.one/', '_blank');
-    window.open('https://jacobleech.com/', '_blank');
-    window.open('https://humanebydesign.com/', '_blank');
-    window.open('https://thebestmotherfucking.website/', '_blank');
-}
+// function otherLinks() {
+//     window.open('https://h.43z.one/', '_blank');
+//     window.open('https://jacobleech.com/', '_blank');
+//     window.open('https://humanebydesign.com/', '_blank');
+//     window.open('https://thebestmotherfucking.website/', '_blank');
+// }
 
 document.getElementById('contact-link').addEventListener('click', function() {
     window.location.href = 'mailto:' + 'ajberquiza' + '@' + 'gmail.com';
